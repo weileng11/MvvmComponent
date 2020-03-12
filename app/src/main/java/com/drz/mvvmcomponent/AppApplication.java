abee5d9bffb7dec737154b1977e578267a57fd00
litao <litao143>
2020-03-12 10:35:47 +0800

    Android 组件化开源app -开眼短视频(OpenEyes)(仅学习收藏,原地址:https://www.wanandroid.com/blog/show/2718)

---
+++ app/src/main/java/com/drz/mvvmcomponent/AppApplication.java
@@ -0,0 +1,29 @@
+package com.drz.mvvmcomponent;
+
+import com.drz.base.base.BaseApplication;
+import com.drz.common.config.ModuleLifecycleConfig;
+import com.zhpan.idea.BuildConfig;
+
+/**
+ * 应用模块: 宿主app
+ * <p>
+ * 类描述: 宿主app的 application ,在这里通过common组件中定义的组件生命周期配置类进行相应初始化(通过反射调用各个组件的初始化器)
+ * <p>
+ *  原文地址:https://www.wanandroid.com/blog/show/2718
+ *  该开源项目采用组件化的方式开发，使用MVVM + AndroidX + jetpack 组件为基本架构进行开发
+ *
+ * @author darryrzhoong
+ * @since 2020-02-26
+ */
+public class AppApplication extends BaseApplication
+{
+    
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        setsDebug(BuildConfig.DEBUG);
+        // 初始化需要初始化的组件
+        ModuleLifecycleConfig.getInstance().initModuleAhead(this);
+    }
+}
